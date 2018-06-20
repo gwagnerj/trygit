4f420a3564f27c12c6b530e5a5b9d5083760442c
wagnerj26 <gwagnerj@gmail.com>
2018-06-20 17:13:44 -0400

    corrected the word template files to reflect the shorter URLs and corrected the problkems QR code so that it goes to the correct place.  THe index files are just redirects

---
+++ index2.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: https://www.QRProblems.org/crud/index.php"); /* Redirect browser to game so we can just use the shorter url */
+exit();
+?>
---
+++ index3.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: https://www.QRProblems.org/crud/QRChecker.php"); /* Redirect browser to game so we can just use the shorter url */
+exit();
+?>
